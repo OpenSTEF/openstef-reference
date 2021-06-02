f93f6e87687d3efe73dec3abbe81a1a16377f0af
Bram Harmsen <bramharmsen@gmail.com>
2021-06-02 21:24:53 +0200

    Move Grafana credentials to docker-compose file

--- grafana-configurator/configurator.sh
+++ grafana-configurator/configurator.sh
@@ -9,9 +9,9 @@ apk add --no-cache curl
 # https://grafana.com/docs/grafana/latest/http_api/data_source/
 for i in ${BASEDIR}/datasources/*; do \
     curl -X "POST" "http://grafana:3000/api/datasources" \
-    -H "Content-Type: application/json" \
-     --user admin:admin \
-     --data-binary @$i
+        -H "Content-Type: application/json" \
+        --user $GRAFANA_USER:$GRAFANA_PASSWORD \
+        --data-binary @$i
 done
 
 # Wraps the dashboard in a JSON message and posts it to the API:
@@ -19,7 +19,7 @@ done
 for i in ${BASEDIR}/dashboards/*.json; do \
     json=`cat ${i}`
     curl -X "POST" "http://grafana:3000/api/dashboards/db" \
-         -H "Content-Type: application/json" \
-         --user admin:admin \
-         --data "{\"dashboard\": ${json}, \"folderId\": 0, \"message\": \"Inital upload\", \"overwrite\": false}"
+        -H "Content-Type: application/json" \
+        --user $GRAFANA_USER:$GRAFANA_PASSWORD \
+        --data "{\"dashboard\": ${json}, \"folderId\": 0, \"message\": \"Inital upload\", \"overwrite\": false}"
 done
