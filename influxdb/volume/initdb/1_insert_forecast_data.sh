044798df2bd826d4d76e239b03a68c2cfcdb6c94
Bram Harmsen <bramharmsen@gmail.com>
2021-06-02 22:51:43 +0200

    Use curl to restore influx, influx cli command is much much slower

--- influxdb/volume/initdb/1_insert_forecast_data.sh
+++ influxdb/volume/initdb/1_insert_forecast_data.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+set -e
+
+FILENAME="/docker-entrypoint-initdb.d/forecast_latest_prediction.txt"
+
+# This is very slow for a lot of data
+# while read line; do
+#     influx -database $FORECAST_DB_NAME -execute "INSERT $line"
+# done < $FILENAME
+
+# Install curl in the Alpine container
+apk add --no-cache curl
+curl -i -XPOST "http://localhost:8086/write?db=forecast_latest&precision=s" \
+    --data-binary "@$FILENAME"
