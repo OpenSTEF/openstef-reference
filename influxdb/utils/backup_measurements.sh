8888a4d32fe9fe3c3fe842ea03899cd96a74a1a6
Bram Harmsen <bramharmsen@gmail.com>
2021-06-02 20:43:41 +0200

    Added influx utility scripts (work in progress)

---
+++ influxdb/utils/backup_measurements.sh
@@ -0,0 +1,101 @@
+
+export INFLUXDB_HOST="localhost"
+export INFLUXDB_PORT="8086"
+export INFLUXDB_FORECAST_DATABASENAME="forecast_latest"
+export INFLUXDB_FORECAST_DATABASE_PREDICTION_SERIES_NAME="prediction"
+# SELECT * FROM "prediction" WHERE ("pid" = '459' OR "pid" = '321' OR "pid" = '317' OR "pid" = '313') AND time >= now() - 7d GROUP BY time(5m), "pid" fill(null)
+
+# 459, 321, 317, 313
+
+# Use this command to make a backup of a production instance
+curl -G 'http://$INFLUXDB_HOST:$INFLUXDB_PORT/query?pretty=true' \
+    --data-urlencode "db=$INFLUXDB_FORECAST_DATABASENAME" \
+    --data-urlencode "q=\
+    SELECT mean(\"forecast\") \
+    FROM \"$INFLUXDB_FORECAST_DATABASE_PREDICTION_SERIES_NAME\" \
+    WHERE (\"pid\" = '459' OR \"pid\" = '321' OR \"pid\" = '317' OR \"pid\" = '313') \
+    AND time >= now() - 7d GROUP BY time(5m) fill(null)" > $INFLUXDB_FORECAST_DATABASENAME.$INFLUXDB_FORECAST_DATABASE_PREDICTION_SERIES_NAME.json
+
+curl -G 'http://$INFLUXDB_HOST:$INFLUXDB_PORT/query?pretty=true' \
+    -H 'Accept:application/csv'\
+    --data-urlencode "db=$INFLUXDB_FORECAST_DATABASENAME" \
+    --data-urlencode "q=\
+    SELECT * FROM \"prediction\"\
+    WHERE (\"pid\" = '459' OR \"pid\" = '321' OR \"pid\" = '317' OR \"pid\" = '313')\
+    AND time >= now() - 1u"
+
+
+
+curl -XPOST -d @forecast_latest.prediction.json "http://$INFLUXDB_HOST:$INFLUXDB_PORT/db/$INFLUXDB_FORECAST_DATABASENAME/$INFLUXDB_FORECAST_DATABASE_PREDICTION_SERIES_NAME"
+
+curl -G 'http://$INFLUXDB_HOST:$INFLUXDB_PORT/query' \
+    -H 'Accept:application/csv'\
+    --data-urlencode "db=$INFLUXDB_FORECAST_DATABASENAME" \
+    --data-urlencode "q=\
+    SELECT mean(\"forecast\") \
+    FROM \"$INFLUXDB_FORECAST_DATABASE_PREDICTION_SERIES_NAME\" \
+    WHERE (\"pid\" = '459' OR \"pid\" = '321' OR \"pid\" = '317' OR \"pid\" = '313') \
+    AND time >= now() - 7d GROUP BY time(5m) fill(null)" > forecast_latest.prediction.json
+
+# InfluxDB API documentation: https://docs.influxdata.com/influxdb/v1.7/tools/api/
+
+# Ping the database
+# curl -sl -I http://localhost:8086/ping
+
+# Export influxDB data into line protocol: influx_inspect
+# https://docs.influxdata.com/influxdb/v1.7/tools/influx_inspect/
+# https://www.influxdata.com/blog/tldr-influxdb-tech-tips-november-3-2016/
+
+
+# get tags:
+
+show tag keys on forecast_latest from prediction
+influx -execute "show tag keys on forecast_latest from prediction"
+curl -GET http://localhost:8086/query?pretty=true \
+        --data-urlencode "db=forecast_latest" \
+        --data-urlencode "q=show tag keys on forecast_latest from prediction"
+
+# get fields:
+
+show field keys on forecast_latest from prediction
+influx -execute "show field keys on forecast_latest from prediction"
+curl -GET http://localhost:8086/query?pretty=true \
+        --data-urlencode "db=forecast_latest" \
+        --data-urlencode "q=show field keys on forecast_latest from prediction"
+
+# get data
+select * from prediction WHERE ("pid" = '459' OR "pid" = '321' OR "pid" = '317' OR "pid" = '313') AND time >= now() - 7d
+influx -database forecast_latest -execute "select * from prediction WHERE ("pid" = '459' OR "pid" = '321' OR "pid" = '317' OR "pid" = '313') AND time >= now() - 7d"
+
+# Making backups
+
+NUM_DAYS="7"
+
+# Backup using `influx` cli
+CLI_ARGS="-format json -precision s -execute"
+# Backup tags
+influx $CLI_ARGS "show tag keys on forecast_latest from prediction" > tags.json
+# Backup fields
+influx $CLI_ARGS "show field keys on forecast_latest from prediction" > fields.json
+# Backup data
+influx -database forecast_latest $CLI_ARGS "\
+    select * from prediction\
+    WHERE ("pid" = '459' OR "pid" = '321' OR "pid" = '317' OR "pid" = '313')\
+    AND time >= now() - ${NUM_DAYS}d limit 1"
+# Backup all at the same time
+influx -database forecast_latest $CLI_ARGS "\
+    show tag keys on forecast_latest from prediction;\
+    show field keys on forecast_latest from prediction;\
+    select * from prediction\
+        WHERE ("pid" = '459' OR "pid" = '321' OR "pid" = '317' OR "pid" = '313')\
+        AND time >= now() - 7d limit 1"
+
+# Backup using `influx` API
+curl -GET http://localhost:8086/query?epoch=s&pretty=true \
+        --data-urlencode "db=forecast_latest" \
+        --data-urlencode "q=\
+            show tag keys on forecast_latest from prediction;\
+            show field keys on forecast_latest from prediction;\
+            select * from prediction\
+                WHERE ("pid" = '459' OR "pid" = '321' OR "pid" = '317' OR "pid" = '313')\
+                AND time >= now() - 7d limit 1" > export.json
---
+++ influxdb/utils/json2line.py
@@ -0,0 +1,144 @@
+"""Convert any InfluxDB 1.x output to line protocol.
+
+Usage:
+    json2line single_export_file.json
+
+    json2line data.json tags.json fields.json
+
+"""
+
+from enum import Enum
+from dataclasses import dataclass
+import sys
+import json
+
+class ColumnType(Enum):
+    TIMESTAMP = "timestamp"
+    TAG = "tag"
+    FIELD = "field"
+
+
+@dataclass
+class Column:
+    name: str
+    type: ColumnType
+    index: int
+
+def convert_data_to_line(data_serie, tags_serie, fields_serie):
+    serie_name = data_serie["name"]
+    column_names = data_serie["columns"]
+
+    tag_names = get_tag_names(tags_serie)
+    field_names = get_field_names(fields_serie)
+
+    columns = determine_columns(column_names, tag_names, field_names)
+    values = data_serie["values"]
+
+    for row in values:
+        line = convert_row_to_line(serie_name, row, columns)
+        print(line)
+
+
+def convert_row_to_line(serie_name, row, columns):
+    tags = {}
+    fields = {}
+    timestamp = None
+    for col in columns:
+        value = row[col.index]
+
+        if value is None:
+            continue
+
+        if col.type is ColumnType.TIMESTAMP:
+            timestamp = value
+
+        if col.type is ColumnType.TAG:
+            tags[col.name] = value
+
+        if col.type is ColumnType.FIELD:
+            fields[col.name] = value
+
+    tag_set = ",".join([f"{k}={v}" for k, v in tags.items()])
+    field_set = ",".join([f"{k}={v}" for k, v in fields.items()])
+    line = f"{serie_name},{tag_set} {field_set} {timestamp}"
+    return line
+
+
+def get_field_names(serie):
+    values = serie["values"]
+    field_names = [v[0] for v in values]
+    return field_names
+
+def get_tag_names(serie):
+    values = serie["values"]
+    tag_names = [v[0] for v in values]
+    return tag_names
+
+
+def determine_columns(column_names, tag_names, field_names):
+    colums = []
+    for i, col_name in enumerate(column_names):
+
+        if col_name == "time":
+            colums.append(Column(name=col_name, index=i, type=ColumnType.TIMESTAMP))
+            continue
+
+        if col_name in tag_names:
+            colums.append(Column(name=col_name, index=i, type=ColumnType.TAG))
+            continue
+
+        if col_name in field_names:
+            colums.append(Column(name=col_name, index=i, type=ColumnType.FIELD))
+            continue
+
+    return colums
+
+
+def parse_single_file(filename):
+    with open(filename) as fh:
+        json_data = json.load(fh)
+    results = json_data["results"]
+
+    if len(results) != 3:
+        raise ValueError("Wrong number of results, expected 3 (data, tags, fields")
+
+    tags_serie = None
+    fields_serie = None
+    data_serie = None
+
+    for result in results:
+        serie = result["series"][0]
+        # we found the tags
+        if "tagKey" in serie["columns"]:
+            tags_serie = serie
+        # we found the fields
+        elif "fieldKey" in serie["columns"]:
+            fields_serie = serie
+        else:
+            data_serie = serie
+
+    return data_serie, tags_serie, fields_serie
+
+
+if __name__ == "__main__":
+
+    # single file mode
+    if len(sys.argv) == 2:
+        data_serie, tags_serie, fields_serie = parse_single_file(sys.argv[1])
+    # separate file mode
+    elif len(sys.argv) == 4:
+        data_file = sys.argv[1]
+        tags_file = sys.argv[2]
+        fields_file = sys.argv[3]
+
+        with open(data_file) as fh:
+            data_serie = json.load(fh)["results"][0]["series"][0]
+
+        with open(tags_file) as fh:
+            tags_serie = json.load(fh)["results"][0]["series"][0]
+
+        with open(fields_file) as fh:
+            fields_serie = json.load(fh)["results"][0]["series"][0]
+
+    convert_data_to_line(data_serie, tags_serie, fields_serie)
+
